4fdb0dc55545d64ebd585c3233338f8967de6bc7
maciek <maciekslotwinski@gmail.com>
2017-10-16 15:23:05 +0200

    added language change button handling

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,40 @@
 'use strict';
-$(document).ready(function(){
-    function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+$(document).ready(function () {
+    function checkScroll() {
+        var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
-    if($(window).scrollTop() > startY){
-        $('.navbar-default').addClass("scrolled");
-    }else{
-        $('.navbar-default').removeClass("scrolled");
+        if ($(window).scrollTop() > startY) {
+            $('.navbar-default').addClass("scrolled");
+        } else {
+            $('.navbar-default').removeClass("scrolled");
+        }
     }
-}
 
-    $(window).on("scroll load resize", function(){
+    $(window).on("scroll load resize", function () {
         console.log('scrolling');
         checkScroll();
     });
 
-    
+
 });
 
 
-$(document).ready(function() {
-    $('#btn-offer').click(function() {  
-         $('#show').slideToggle('slow');  
+$(document).ready(function () {
+    $('#btn-offer').click(function () {
+        $('#show').slideToggle('slow');
     });
-        
-    
+
+
 });
 
 
-$(document).ready(function() {
-    $('div.language').click(function() {
-        if($('div.language span').html() === 'EN'){
+$(document).ready(function () {
+    $('div.language').click(function () {
+        if ($('div.language span').html() === 'EN') {
             var opts = {language: "en", pathPrefix: "lang"};
             $("[data-localize]").localize("softCreation", opts);
             $('div.language span').html('PL');
-        } else if ($('div.language span').html() === 'PL'){
+        } else if ($('div.language span').html() === 'PL') {
             var opts = {language: "pl", pathPrefix: "lang"};
             $("[data-localize]").localize("softCreation", opts);
             $('div.language span').html('EN');
@@ -42,13 +42,7 @@ $(document).ready(function() {
         }
 
 
-
     });
-
-
-
-
-
 
 
 });
