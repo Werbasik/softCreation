402c9e279513d14cc775d487c43568d67ef5bb6c
maciek <maciekslotwinski@gmail.com>
2017-10-18 11:28:32 +0200

    refactored localization function, added language detection

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,9 @@
 'use strict';
 $(document).ready(function () {
+
+    // check if users scrolls down for changing navbar style
     function checkScroll() {
-        var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+        var startY = $('.navbar').height() * 2; //Point where nav changes style
 
         if ($(window).scrollTop() > startY) {
             $('.navbar-default').addClass("scrolled");
@@ -16,6 +18,7 @@ $(document).ready(function () {
     });
 });
 
+// Handle reveal more text in offer section
 $(document).ready(function () {
     $('#btn-offer').click(function () {
         $('#show').slideToggle('slow');
@@ -23,33 +26,88 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
+
+    console.log(navigator.language);
+
+    if(navigator.language != 'pl'){
+        switchLanguage('en');
+        localizaFormToEn();
+    } else {
+        switchLanguage('pl');
+        localizeFormToPl();
+    }
+
+
+
+
     $('div.language').click(function () {
         if ($('div.language span').html() === 'EN') {
-            var opts = {language: "en", pathPrefix: "lang"};
-            $("[data-localize]").localize("softCreation", opts);
-            $('div.language span').html('PL');
-            // localiztaion plugin does not cover attributes, translating form manually
-            $('#contactFormName').attr("placeholder","Name");
-            $('#contactFormPhone').attr("placeholder","Phone");
-            $('#contactFormMessage').attr("placeholder","Message");
-            $('#contactFormSubmit').attr("value","Send");
+            console.log('klik on EN!');
+            switchLanguage('en');
+            localizaFormToEn();
 
 
         } else if ($('div.language span').html() === 'PL') {
-            console.log('click on PL!');
-            var opts = {language: "pl", pathPrefix: "lang"};
-            $("[data-localize]").localize("softCreation", opts);
-            $('div.language span').html('EN');
-            // localiztaion plugin does not cover attributes, translating form manually
-            $('#contactFormName').attr("placeholder","Imię");
-            $('#contactFormPhone').attr("placeholder","Telefon");
-            $('#contactFormMessage').attr("placeholder","Wiadomość");
-            $('#contactFormSubmit').attr("value","Wyślij");
+            console.log('klik on PL!');
+            switchLanguage('pl');
+            localizeFormToPl();
         }
 
 
     });
 
+
+
+
+
+
+
+
+    // $('div.language').click(function () {
+    //     if ($('div.language span').html() === 'EN') {
+    //         var opts = {language: "en", pathPrefix: "lang"};
+    //         $("[data-localize]").localize("softCreation", opts);
+    //         $('div.language span').html('PL');
+    //         // localiztaion plugin does not cover attributes, translating form manually
+    //         $('#contactFormName').attr("placeholder","Name");
+    //         $('#contactFormPhone').attr("placeholder","Phone");
+    //         $('#contactFormMessage').attr("placeholder","Message");
+    //         $('#contactFormSubmit').attr("value","Send");
+    //
+    //
+    //     } else if ($('div.language span').html() === 'PL') {
+    //         console.log('click on PL!');
+    //         var opts = {language: "pl", pathPrefix: "lang"};
+    //         $("[data-localize]").localize("softCreation", opts);
+    //         $('div.language span').html('EN');
+    //         // localiztaion plugin does not cover attributes, translating form manually
+    //         $('#contactFormName').attr("placeholder","Imię");
+    //         $('#contactFormPhone').attr("placeholder","Telefon");
+    //         $('#contactFormMessage').attr("placeholder","Wiadomość");
+    //         $('#contactFormSubmit').attr("value","Wyślij");
+    //     }
+    //
+    //
+    // });
+
+    function switchLanguage(languageCode){
+        var opts = {language: languageCode, pathPrefix: "lang"};
+        $("[data-localize]").localize("softCreation", opts);
+        $('div.language span').html(languageCode.toUpperCase());
+    }
+
+    function localizeFormToPl(){
+        $('#contactFormName').attr("placeholder","Imię");
+        $('#contactFormPhone').attr("placeholder","Telefon");
+        $('#contactFormMessage').attr("placeholder","Wiadomość");
+        $('#contactFormSubmit').attr("value","Wyślij");
+    }
+    function localizaFormToEn(){
+        $('#contactFormName').attr("placeholder","Name");
+        $('#contactFormPhone').attr("placeholder","Phone");
+        $('#contactFormMessage').attr("placeholder","Message");
+        $('#contactFormSubmit').attr("value","Send");
+    }
 
 });
 
